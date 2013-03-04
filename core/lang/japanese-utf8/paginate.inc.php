ffb3e906f99673114b5ae0a0502fa641a14af752
yama <yamamoto@kyms.jp>
2013-03-04 09:29:02 +0900

    Add Japanese language files

---
+++ core/lang/japanese-utf8/paginate.inc.php
@@ -0,0 +1,10 @@
+<?php
+if(!defined('MODX_BASE_PATH')){die('What are you doing? Get out of here!');}
+
+setlocale (LC_ALL, 'ja_JP.UTF-8');
+
+$_lang=array();
+$_lang['paginate.next']='次へ';
+$_lang['paginate.prev']='戻る';
+
+return $_lang;
