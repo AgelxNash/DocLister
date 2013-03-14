56f617caa5dd6b321c17e3e0a64a6ce36651fc90
AgelxNash <iam@agel-nash.ru>
2013-03-14 19:04:53 +0600

    [Fix] int convert to empty

--- core/DocLister.class.php
+++ core/DocLister.class.php
@@ -635,7 +635,7 @@ abstract class DocLister {
      * @return string Clear string
      */
 	final public function sanitarData($data){
-        return is_string($data) ? str_replace(array('[', '%5B', ']', '%5D','{','%7B','}','%7D'), array('&#91;', '&#91;', '&#93;', '&#93;','&#123;','&#123;','&#125;','&#125;'),htmlspecialchars($data)) : '';	}
+        return is_scalar($data) ? str_replace(array('[', '%5B', ']', '%5D','{','%7B','}','%7D'), array('&#91;', '&#91;', '&#93;', '&#93;','&#123;','&#123;','&#125;','&#125;'),htmlspecialchars($data)) : '';	}
     /*
      * run tree build
      *
