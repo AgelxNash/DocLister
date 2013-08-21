48adfffd75672d378a8bea63cfc9c13925642f31
AgelxNash <iam@agel-nash.ru>
2013-08-21 14:25:13 +0600

    Fix sort crumb

--- snippet.DLcrumbs.php
+++ snippet.DLcrumbs.php
@@ -8,18 +8,19 @@
  * @version 1.0.20
  */
 if (!defined('MODX_BASE_PATH')) {
-    die('HACK???');
+	die('HACK???');
 }
 $_parents=$modx->getParentIds($modx->documentObject['id']);
+$_parents=array_reverse(array_values($_parents));
 $_parents[] = $modx->documentObject['id'];
 
 $_options = array(
-    'tpl'=>'@CODE:<li><a href="[+url+]" title="[+title+]">[+title+]</a></li>',
-    'tplCurrent'=>'@CODE:<li><span>[+title+]</span></li>',
-    'ownerTPL'=>'@CODE:<nav class="breadcrumbs"><ul>[+crumbs.wrap+]</ul></nav>',
-    'sortType' => 'doclist',
-    'sysKey'=>'crumbs',
-    'documents'=>implode(",",array_values($_parents))
+	'tpl'=>'@CODE:<li><a href="[+url+]" title="[+title+]">[+title+]</a></li>',
+	'tplCurrent'=>'@CODE:<li><span>[+title+]</span></li>',
+	'ownerTPL'=>'@CODE:<nav class="breadcrumbs"><ul>[+crumbs.wrap+]</ul></nav>',
+	'sortType' => 'doclist',
+	'sysKey'=>'crumbs',
+	'documents'=>implode(",",$_parents)
 );
 
 $_options = array_merge($_options, $modx->event->params);
