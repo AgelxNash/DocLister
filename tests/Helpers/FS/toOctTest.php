0e875d1ca50a764304976c10ffbcbe7f099f3000
AgelxNash <agel_nash@xaker.ru>
2015-02-12 17:58:17 +0500

    Отрицательные тесты для \Helpers\FS::toOct

--- tests/Helpers/FS/toOctTest.php
+++ tests/Helpers/FS/toOctTest.php
@@ -9,18 +9,22 @@ class toOctTest extends \PHPUnit_Framework_TestCase {
 		$this->FS = FS::getInstance();
 	}
 	public function test8StringTo8Success(){
+		$this->assertNotEquals(0755, '0755');
 		$this->assertEquals(0755, $this->FS->toOct('0755'));
 	}
 
 	public function test8to8Success(){
+		$this->assertEquals(0755, 0755);
 		$this->assertEquals(0755, $this->FS->toOct(0755));
 	}
 
 	public function test10to8Success(){
+		$this->assertNotEquals(0755, 755);
 		$this->assertEquals(0755, $this->FS->toOct(755));
 	}
 
 	public function test10StringTo8Success(){
+		$this->assertNotEquals(0755, '755');
 		$this->assertEquals(0755, $this->FS->toOct('755'));
 	}
 }
