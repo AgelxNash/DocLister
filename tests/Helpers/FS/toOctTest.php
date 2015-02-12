15788b0aa1af2dd6d6c2d0f2f28a1ceb7bc1aa41
AgelxNash <agel_nash@xaker.ru>
2015-02-12 17:56:00 +0500

    PHPUnit тесты для баг-репорта #123

---
+++ tests/Helpers/FS/toOctTest.php
@@ -0,0 +1,26 @@
+<?php namespace DocLister\Tests\Helpers\FS;
+
+use \Helpers\FS;
+
+class toOctTest extends \PHPUnit_Framework_TestCase {
+	protected $FS = null;
+
+	public function setUp(){
+		$this->FS = FS::getInstance();
+	}
+	public function test8StringTo8Success(){
+		$this->assertEquals(0755, $this->FS->toOct('0755'));
+	}
+
+	public function test8to8Success(){
+		$this->assertEquals(0755, $this->FS->toOct(0755));
+	}
+
+	public function test10to8Success(){
+		$this->assertEquals(0755, $this->FS->toOct(755));
+	}
+
+	public function test10StringTo8Success(){
+		$this->assertEquals(0755, $this->FS->toOct('755'));
+	}
+}
