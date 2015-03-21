3cd729e2e5d674592ba077c6685f997b3394f777
AgelxNash <agel_nash@xaker.ru>
2015-03-21 05:53:13 +0500

    DLpaginate refactor

--- assets/snippets/DocLister/lib/DLpaginate.class.php
+++ assets/snippets/DocLister/lib/DLpaginate.class.php
@@ -147,6 +147,7 @@ class DLpaginate {
                 $out = $display * ($page - 1);
                 break;
             }
+            case 'back':
             case 'pages':
             default:{
                 $out = $page;
@@ -176,20 +177,19 @@ class DLpaginate {
 
         if ($this->urlF and $this->urlF != '%' and strpos($this->target, $this->urlF) === false) {
             //Es necesario especificar el comodin para sustituir
-            echo "Especificaste un wildcard para sustituir, pero no existe en el target<br />";
+            //echo "Especificaste un wildcard para sustituir, pero no existe en el target<br />";
             $error = true;
         } elseif ($this->urlF and $this->urlF == '%' and strpos($this->target, $this->urlF) === false) {
-
-            echo "Es necesario especificar en el target el comodin % para sustituir el n�mero de p�gina<br />";
+            //echo "Es necesario especificar en el target el comodin % para sustituir el n�mero de p�gina<br />";
             $error = true;
         }
 
         if ($this->total_pages < 0) {
-            echo "It is necessary to specify the <strong>number of pages</strong> (\$class->items(1000))<br />";
+            //echo "It is necessary to specify the <strong>number of pages</strong> (\$class->items(1000))<br />";
             $error = true;
         }
         if ($this->limit == null) {
-            echo "It is necessary to specify the <strong>limit of items</strong> to show per page (\$class->limit(10))<br />";
+            //echo "It is necessary to specify the <strong>limit of items</strong> to show per page (\$class->limit(10))<br />";
             $error = true;
         }
         if ($error) return false;
@@ -211,71 +211,71 @@ class DLpaginate {
                 We're actually saving the code to a variable in case we want to draw it more than once.
         */
         if ($lastpage > 1) {
-
             if ($this->page) {
-                //anterior button
                 if ($this->page > 1) {
-                    $this->pagination .= $this->prevT ? str_replace(array('[+link+]', '[+num+]'), array($this->get_pagenum_link($prev), ($this->page-1)), $this->prevT) : ''; // "<a href=\"".$this->get_pagenum_link($prev)."\" class=\"prev\">$p</a>";
-                } else
-                    $this->pagination .= $this->prevI ? str_replace(array('[+link+]', '[+num+]'), array($this->get_pagenum_link($prev), 0), $this->prevI) : '';
-                //"<span class=\"disabled\">$p</span>";
-
+                    $this->pagination .=  $this->prevT ? $this->renderItemTPL( $this->prevT, $prev) : '';
+                } else {
+                    $this->pagination .=  $this->prevI ? $this->renderItemTPL( $this->prevI, $prev) : '';
+                }
             }
             //pages
             if ($lastpage < 7 + ($this->adjacents * 2)) { //not enough pages to bother breaking it up
                 for ($counter = 1; $counter <= $lastpage; $counter++) {
-                    if ($counter == $this->page)
-                        $this->pagination .= str_replace('[+num+]', $counter, str_replace('[+link+]', $this->get_pagenum_link($this->page), $this->currentT)); // "<span class=\"current\"></span>";
-                    else
-                        $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link($counter), str_replace('[+num+]', $counter, $this->numberT)); //"<a href=\"".$this->get_pagenum_link($counter)."\">$counter</a>";
+                    $tpl = ($counter == $this->page) ? $this->currentT : $this->numberT;
+                    $this->pagination .= $this->renderItemTPL($tpl, $counter);
                 }
             } elseif ($lastpage > 5 + ($this->adjacents * 2)) { //enough pages to hide some
                 //close to beginning; only hide later pages
                 if ($this->page < 1 + ($this->adjacents * 2)) {
                     for ($counter = 1; $counter < 4 + ($this->adjacents * 2); $counter++) {
-                        if ($counter == $this->page)
-                            $this->pagination .= str_replace('[+num+]', $counter, str_replace('[+link+]', $this->get_pagenum_link($this->page), $this->currentT));
-                        else
-                            $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link($counter), str_replace('[+num+]', $counter, $this->numberT));
+                        $tpl = ($counter == $this->page) ? $this->currentT : $this->numberT;
+                        $this->pagination .= $this->renderItemTPL($tpl, $counter);
                     }
-                    $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link($counter), $this->dotsT);
-                    $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link($lpm1), str_replace('[+num+]', $lpm1, $this->numberT));
-                    $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link($lastpage), str_replace('[+num+]', $lastpage, $this->numberT));
+                    $this->pagination .= $this->renderItemTPL($this->dotsT, $counter);
+                    $this->pagination .= $this->renderItemTPL($this->numberT, $lpm1);
+                    $this->pagination .= $this->renderItemTPL($this->numberT, $lastpage);
                 } //in middle; hide some front and some back
                 elseif ($lastpage - ($this->adjacents * 2) > $this->page && $this->page > ($this->adjacents * 2)) {
-                    $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link(1), str_replace('[+num+]', 1, $this->numberT)); //     "<a href=\"".$this->get_pagenum_link(1)."\">1</a>";
-                    $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link(2), str_replace('[+num+]', 2, $this->numberT));
-                    $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link(3), $this->dotsT);
-                    for ($counter = $this->page - $this->adjacents; $counter <= $this->page + $this->adjacents; $counter++)
-                        if ($counter == $this->page)
-                            $this->pagination .= str_replace('[+num+]', $counter, str_replace('[+link+]', $this->get_pagenum_link($this->page), $this->currentT));
-                        else
-                            $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link($counter), str_replace('[+num+]', $counter, $this->numberT)); //"<a href=\"".$this->get_pagenum_link($counter)."\">$counter</a>";
-                    $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link($counter), $this->dotsT);
-                    $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link($lpm1), str_replace('[+num+]', $lpm1, $this->numberT)); // "<a href=\"".$this->get_pagenum_link($lpm1)."\">$lpm1</a>";
-                    $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link($lastpage), str_replace('[+num+]', $lastpage, $this->numberT)); //"<a href=\"".$this->get_pagenum_link($lastpage)."\">$lastpage</a>";
+                    $this->pagination .= $this->renderItemTPL($this->numberT, 1);
+                    $this->pagination .= $this->renderItemTPL($this->numberT, 2);
+                    $this->pagination .= $this->renderItemTPL($this->dotsT, 3);
+
+                    for ($counter = $this->page - $this->adjacents; $counter <= $this->page + $this->adjacents; $counter++){
+                        $tpl = ($counter == $this->page) ? $this->currentT : $this->numberT;
+                        $this->pagination .= $this->renderItemTPL($tpl, $counter);
+                    }
+                    $this->pagination .= $this->renderItemTPL($this->dotsT, $counter);
+                    $this->pagination .= $this->renderItemTPL($this->numberT, $lpm1);
+                    $this->pagination .= $this->renderItemTPL($this->numberT, $lastpage);
                 } //close to end; only hide early pages
                 else {
-                    $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link(1), str_replace('[+num+]', 1, $this->numberT)); //"<a href=\"".$this->get_pagenum_link(1)."\">1</a>";
-                    $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link(2), str_replace('[+num+]', 2, $this->numberT)); //"<a href=\"".$this->get_pagenum_link(2)."\">2</a>";
-                    $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link(3), $this->dotsT);
-                    for ($counter = $lastpage - (2 + ($this->adjacents * 2)); $counter <= $lastpage; $counter++)
-                        if ($counter == $this->page)
-                            $this->pagination .= str_replace('[+num+]', $counter, str_replace('[+link+]', $this->get_pagenum_link($this->page), $this->currentT));
-                        else
-                            $this->pagination .= str_replace('[+link+]', $this->get_pagenum_link($counter), str_replace('[+num+]', $counter, $this->numberT));
+                    $this->pagination .= $this->renderItemTPL($this->numberT, 1);
+                    $this->pagination .= $this->renderItemTPL($this->numberT, 2);
+                    $this->pagination .= $this->renderItemTPL($this->dotsT, 3);
+
+                    for ($counter = $lastpage - (2 + ($this->adjacents * 2)); $counter <= $lastpage; $counter++){
+                        $tpl = ($counter == $this->page) ? $this->currentT : $this->numberT;
+                        $this->pagination .= $this->renderItemTPL($tpl, $counter);
+                    }
                 }
             }
             if ($this->page) {
-                //siguiente button
-                if ($this->page < $counter - 1)
-                    $this->pagination .= $this->nextT ? str_replace(array('[+link+]', '[+num+]'), array($this->get_pagenum_link($next), ($this->page+1)), $this->nextT) : ''; //"<a href=\"".$this->get_pagenum_link($next)."\" class=\"next\">$n</a>";
-                else
-                    $this->pagination .= $this->nextI ? str_replace(array('[+link+]', '[+num+]'), array($this->get_pagenum_link($next), 0), $this->nextI) : '';
-                if ($this->showCounter) $this->pagination .= "<div class=\"pagination_data\">($this->total_pages Pages)</div>";
+                if ($this->page < $counter - 1){
+                    $this->pagination .=  $this->nextT ? $this->renderItemTPL( $this->nextT, $next) : '';
+                } else {
+                    $this->pagination .=  $this->nextI ? $this->renderItemTPL( $this->nextI, $next) : '';
+                }
+
+                if ($this->showCounter) {
+                    $this->pagination .= "<div class=\"pagination_data\">($this->total_pages Pages)</div>";
+                }
             }
         }
 
         return true;
+    }
+
+    protected function renderItemTPL($tpl, $num){
+        return str_replace(array('[+num+]', '[+link+]'), array($num, $this->get_pagenum_link($num)), $tpl);
     }
 }
