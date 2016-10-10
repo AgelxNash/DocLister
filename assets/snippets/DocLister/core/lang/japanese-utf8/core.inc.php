bd6c1478fb295ed01948a99659ce6870ed75d175
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-10-10 02:38:09 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- assets/snippets/DocLister/core/lang/japanese-utf8/core.inc.php
+++ assets/snippets/DocLister/core/lang/japanese-utf8/core.inc.php
@@ -1,5 +1,7 @@
 <?php
-if ( ! defined('MODX_BASE_PATH')) die('What are you doing? Get out of here!');
+if ( ! defined('MODX_BASE_PATH')) {
+    die('What are you doing? Get out of here!');
+}
 
 $_lang = array();
 $_lang['hello'] = 'こんにちは';
