7622f8632c14077bbf812a32fe75bf55043cee20
Pathologic <maxx@np.by>
2016-10-26 01:35:00 +0300

    remove spaces from inline templates

--- assets/snippets/DocLister/snippet.DLBuildMenu.php
+++ assets/snippets/DocLister/snippet.DLBuildMenu.php
@@ -17,12 +17,12 @@ $p['currentDepth'] = $currentDepth = \APIhelpers::getkey($p, 'currentDepth', 1);
 
 /** Основной шаблон обертка */
 $p['TplMainOwner'] = \APIhelpers::getkey($p, 'TplMainOwner',
-    '@CODE: <ul id="nav" class="menu level-1">[+dl.wrap+]</ul>'
+    '@CODE:<ul id="nav" class="menu level-1">[+dl.wrap+]</ul>'
 );
 
 /** Шаблон обертка для вложенных уровней */
 $p['TplSubOwner'] = \APIhelpers::getkey($p, 'TplSubOwner',
-    '@CODE: <ul class="sub-menu level-[+dl.currentDepth+]">[+dl.wrap+]</ul>'
+    '@CODE:<ul class="sub-menu level-[+dl.currentDepth+]">[+dl.wrap+]</ul>'
 );
 
 /**
@@ -38,7 +38,7 @@ if (empty($currentOwnerTpl)) {
 
 /** Основной шаблон для каждого пункта меню всех уровней */
 $p['TplOneItem'] = $currentTpl = \APIhelpers::getkey($p, 'TplOneItem',
-    '@CODE: <li id="menu-item-[+id+]" class="menu-item [+dl.class+]">
+    '@CODE:<li id="menu-item-[+id+]" class="menu-item [+dl.class+]">
         <a href="[+url+]" title="[+e.title+]">[+title+]</a>
         [+dl.submenu+]
     </li>'
