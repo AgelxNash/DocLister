d8c3d8899a3ae3bb7275e6629e0b5a5243b1892c
Сергей Савельев <sergej@eyrini.ru>
2017-04-08 09:43:35 +0300

    Update site_content.php

--- assets/snippets/DocLister/core/controller/site_content.php
+++ assets/snippets/DocLister/core/controller/site_content.php
@@ -329,11 +329,11 @@ class site_contentDocLister extends DocLister
             $sort = $this->SortOrderSQL("if(c.pub_date=0,c.createdon,c.pub_date)");
             list($from) = $this->injectSortByTV($from, $sort);
             
-            if ( $this->getCFGDef('idType', 'parents') == 'parents' || $this->getCFGDef('filters') ){
+            if ( $this->getCFGDef('idType') == 'parents' || $this->getCFGDef('ignoreEmpty') || $this->getCFGDef('filters') || $this->getCFGDef('addWhereList') ){
                 $rs = $this->dbQuery("SELECT count(*) FROM (SELECT count(*) FROM {$from} {$where} {$group}) as `tmp`");
                 $out = $this->modx->db->getValue($rs);
             }
-            else{
+            else {
                 $out = count($this->IDs);
             }
         }
