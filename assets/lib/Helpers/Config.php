8883ace4c1f27087dd9c1f134f4c3448c8ac7fee
Pathologic <maxx@np.by>
2017-11-15 08:13:01 +0300

    fix Helpers\Config

--- assets/lib/Helpers/Config.php
+++ assets/lib/Helpers/Config.php
@@ -117,10 +117,12 @@ class Config
 
         if (is_scalar($arr)) {
             $out = \jsonHelper::jsonDecode($arr, array('assoc' => true));
-            if (is_null($out) && $sep) {
-                $out = array_filter(explode($sep, $arr));
-            } else {
-                $out = array();
+            if (!is_array($out)) {
+                if ($sep) {
+                    $out = array_filter(explode($sep, $arr));
+                } else {
+                    $out = array();
+                }
             }
 
             return $out;
